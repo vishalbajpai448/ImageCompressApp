443f1e5e4086bc8d13ed3a5a76222f43c21d3078
Vishal Bajpai <vishal.bajpai448@gmail.com>
2024-02-23 10:40:34 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxImageCompressService } from 'ngx-image-compress';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'imageCompress';
-}
+  compressedImageResult!: string | ArrayBuffer;
+  constructor(private imageCompress: NgxImageCompressService) {}
+
+    imgResultBeforeCompression: string = '';
+    imgResultAfterCompression: string = '';
+
+    compressFile() {
+        this.imageCompress.uploadFile().then(({image, orientation}) => {
+            this.imgResultBeforeCompression = image;
+            console.log('Size in bytes of the uploaded image was:', this.imageCompress.byteCount(image));
+
+            this.imageCompress
+                .compressFile(image, orientation, 50, 50) // 50% ratio, 50% quality
+                .then(compressedImage => {
+                    this.imgResultAfterCompression = compressedImage;
+                    this.compressedImageResult = compressedImage;
+                    console.log('Size in bytes after compression is now:', this.imageCompress.byteCount(compressedImage));
+                });
+        });
+    }
+    downloadCompressedImage(): void {
+      const link = document.createElement('a');
+      link.href = this.compressedImageResult as string;
+      link.download = 'compressed_image.jpg';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgxImageCompressService} from 'ngx-image-compress';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    NgxImageCompressService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
